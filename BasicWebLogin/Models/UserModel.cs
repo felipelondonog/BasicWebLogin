36f4e4f978db413c2800a91dc893033213a72a1a
Felipe Londoño <felipe12091@hotmail.com>
2024-07-24 14:27:30 -0500

    Created UserModel.cs

---
+++ BasicWebLogin/Models/UserModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BasicWebLogin.Models
+{
+    public class UserModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Token { get; set; }
+
+        public string UserName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public bool EmailConfirmed { get; set; }
+
+        public bool ResetPassword { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public bool PhoneNumberConfirmed { get; set; }
+    }
+}
