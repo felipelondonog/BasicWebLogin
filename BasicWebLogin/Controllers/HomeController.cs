5b7b1b737b5581b9e0371aa3a325a97df5420a94
Felipe Londoño <felipe12091@hotmail.com>
2024-07-28 13:59:38 -0500

    Added confirm email functionality

--- BasicWebLogin/Controllers/HomeController.cs
+++ BasicWebLogin/Controllers/HomeController.cs
@@ -3,19 +3,37 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using System.Diagnostics;
+using BasicWebLogin.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BasicWebLogin.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly LoginDBContext _context; 
         private readonly ILogger<HomeController> _logger;
 
-        public IActionResult Index()
+        public HomeController(LoginDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
         {
             if (HttpContext.User.Claims.FirstOrDefault() == null)
             {
                 return RedirectToAction("LogIn", "LogIn");
             }
+
+            string? email = HttpContext.User.Claims.FirstOrDefault().Value;
+
+            // Checks if user has email confirmed
+            UserModel? user = await _context.UserModels.FirstOrDefaultAsync(u => u.Email == email);
+            if (user.EmailConfirmed == false)
+            {
+                return RedirectToAction("LogIn", "LogIn");
+            }
+
             ViewData["SessionUser"] = HttpContext.User.Claims.FirstOrDefault().Value;
             return View();
         }
--- BasicWebLogin/Controllers/LogInController.cs
+++ BasicWebLogin/Controllers/LogInController.cs
@@ -1,4 +1,5 @@
 ﻿using BasicWebLogin.Models;
+using BasicWebLogin.Services;
 using BasicWebLogin.Utilities;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
@@ -36,6 +37,12 @@ namespace BasicWebLogin.Controllers
                 if (user == null)
                 {
                     ViewBag.Message = "User not found.";
+                    return View(loginModel);
+                }
+
+                if (!user.EmailConfirmed)
+                {
+                    ViewBag.Message = "You have to confirm your email to continue.";
                     return View(loginModel);
                 }
 
@@ -81,38 +88,84 @@ namespace BasicWebLogin.Controllers
         {
             try
             {
-                //if (ModelState.IsValid)
-                //{
-                    // Checks the password are the same before saving changes
-                    if (loginModel.Pwd != loginModel.ConfirmPassword)
-                    {
-                        ViewBag.Message = "The passwords do not match.";
-                        return View(loginModel);
-                    }
+                // Checks the password are the same before saving changes
+                if (loginModel.Pwd != loginModel.ConfirmPassword)
+                {
+                    ViewBag.Message = "The passwords do not match.";
+                    return View(loginModel);
+                }
 
-                    // Checks the user email is not already used.
-                    if (await _context.UserModels.FirstOrDefaultAsync(u => u.Email == loginModel.Email) != null)
-                    {
-                        ViewBag.Message = "The email is already registered.";
-                        return View(loginModel);
-                    }
+                // Checks the user email is not already used.
+                if (await _context.UserModels.FirstOrDefaultAsync(u => u.Email == loginModel.Email) != null)
+                {
+                    ViewBag.Message = "The email is already registered.";
+                    return View(loginModel);
+                }
 
-                    UserModel user = loginModel;
-                    // Creates a new Token for the user
-                    user.Token = GeneralUtilities.CreateToken();
+                UserModel user = loginModel;
+                // Creates a new Token for the user
+                user.Token = GeneralUtilities.CreateToken();
 
-                    // Encodes the password before saving
-                    user.Pwd = GeneralUtilities.ConvertStringtoSHA256(user.Pwd);
-                    user.ResetPassword = false;
-                    user.EmailConfirmed = false;
+                // Encodes the password before saving
+                user.Pwd = GeneralUtilities.ConvertStringtoSHA256(user.Pwd);
+                user.ResetPassword = false;
+                user.EmailConfirmed = false;
 
-                    _context.UserModels.Add(user);
-                    await _context.SaveChangesAsync();
-                ViewBag.Message = "Your account is disabled until you confirm your account, please check your email.";
-                    return RedirectToAction("LogIn", "LogIn");
-                //}
+                await SendConfirmationEmail(user);
 
-                //return View(loginModel);
+                _context.UserModels.Add(user);
+                await _context.SaveChangesAsync();
+                TempData["ConfirmMessage"] = "Your account is disabled until you confirm your account, please check your email.";
+                TempData.Keep();
+                return RedirectToAction("LogIn", "LogIn");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+                return View(loginModel);
+            }
+        }
+
+        public ActionResult ConfirmEmail(string token)
+        {
+            try
+            {
+                // Changes EmailConfirmed state on DB
+                int n = _context.UserModels
+                    .Where(u => u.Token == token)
+                    .ExecuteUpdate(u => u.SetProperty(p => p.EmailConfirmed, p => true));
+
+                if (n == 0) ViewBag.Response = false;
+                else ViewBag.Response = true;
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "We're sorry, we couldn't confirm your email, please try again. " + ex.Message;
+                return RedirectToAction("LogIn", "LogIn");
+            }
+        }
+
+        private async Task<IActionResult> SendConfirmationEmail(UserModel user)
+        {
+            try
+            {
+                // Sends confirmation E-mail
+                // Define the path where the template is located and gets the file content
+                string path = "Templates/ConfirmEmail.html";
+                string content = System.IO.File.ReadAllText(path);
+                // Builds the url and replaces it into the content
+                string url = String.Format("{0}://{1}{2}", Request.Scheme, Request.Headers["Host"], "/LogIn/ConfirmEmail?token=" + user.Token);
+                string htmlBody = String.Format(content, user.UserName, url);
+                // Sends the email
+                await EmailService.SendEmail(new EmailModel()
+                {
+                    To = user.Email,
+                    Subject = "Confirm your account",
+                    Content = htmlBody
+                });
+
+                return Ok();
             }
             catch (Exception ex)
             {
